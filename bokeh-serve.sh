c17d173825ae81c46ba3eb937db14f016c3b9937
Daniel Kapitan <dkapitan>
2020-01-16 12:43:38 +0100

    add Dockerfile based on python:3.7-slim
    - add serve.sh for testing

---
+++ bokeh-serve.sh
@@ -0,0 +1,20 @@
+bokeh serve --port 8080 \
+    --allow-websocket-origin="*" \
+    --index=/Users/dkapitan/git/demo.bokeh.org/index.html \
+    ./examples/app/clustering \
+    ./examples/app/crossfilter \
+    ./examples/app/dash \
+    ./examples/app/export_csv \
+    ./examples/app/fourier_animated.py \
+    ./examples/app/gapminder \
+    ./examples/app/image_blur.py \
+    ./examples/app/movies \
+    ./examples/app/ohlc \
+    ./examples/app/population.py \
+    ./examples/app/selection_histogram.py \
+    ./examples/app/sliders.py \
+    ./examples/app/spectrogram \
+    ./examples/app/surface3d \
+    ./examples/app/stocks \
+    ./examples/app/taylor.py \
+    ./examples/app/weather
